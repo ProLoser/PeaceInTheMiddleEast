f7f04059054d026b5ce118ddbff155c95173d1ca
Dean Sofer <Dean.Sofer@sony.com>
2024-05-20 21:24:30 +0300

    Installable and works offline

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: false
+      },
+      manifest: {
+        theme_color: '#ed7560',
+        icons: [
+          {
+            src: 'android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+})
+  ],
   base: '/PeaceInTheMiddleEast/'
 })
