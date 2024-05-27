7ebf480e0e8b81b6988313f6da4a2f5114b62fa8
Dean Sofer <Dean.Sofer@sony.com>
2024-05-27 13:07:03 +0400

    Hide fullscreen button when not available

--- src/Game/Toolbar.tsx
+++ src/Game/Toolbar.tsx
@@ -10,7 +10,7 @@ export default function Toolbar() {
     const toggleChat = useCallback(() => {    }, [])
     const toggleAccount = useCallback(() => {    }, [])
     return <div id="toolbar">
-        <a onClick={toggleFullscreen}>&#x26F6;</a>
+        {document.fullscreenEnabled ? <a onClick={toggleFullscreen}>&#x26F6;</a> : null}
         <a onClick={toggleChat}>&#128488;</a>
         <a onClick={toggleAccount}>&#127757;</a>
     </div>
