fa0c62ff079024c225dc6f9cfa8cd6a60bd7cce1
Dean Sofer <Dean.Sofer@sony.com>
2024-05-27 15:58:40 +0300

    Added piece stacking

--- src/Game/Dice.tsx
+++ src/Game/Dice.tsx
@@ -8,7 +8,7 @@ type DiceProps = {
 }
 
 export default function Dice({ onClick, values }: DiceProps) {
-    return <div className="dice" onClick={onClick} style={{ cursor: 'pointer' }}>
+    return <div className="dice" onClick={onClick}>
         <img src={IMAGES[`black${values[0] || 6}`]} />
         <img src={IMAGES[`white${values[1] || 6}`]} />
     </div>
