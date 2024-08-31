328761d35f408536dcdc901a565e653bf9434153
Dean Sofer <ProLoser@users.noreply.github.com>
2024-08-31 20:14:58 +0000

    Enable more authentication

--- src/Friends/index.tsx
+++ src/Friends/index.tsx
@@ -17,8 +17,10 @@ const uiConfig = {
     signInFlow: 'popup',
     // We will display Google and Facebook as auth providers.
     signInOptions: [
-        // firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-        // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+        firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+        firebase.auth.EmailAuthProvider.PROVIDER_ID,
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        firebase.auth.FacebookAuthProvider.PROVIDER_ID,
         firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
     ],
     callbacks: {
