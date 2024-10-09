a649a4d57929d11e4351d878fb523dae8a537508
Dean Sofer <ProLoser@users.noreply.github.com>
2024-10-09 17:15:31 +0000

    Try to prevent double onClick

--- src/Avatar.tsx
+++ src/Avatar.tsx
@@ -7,7 +7,5 @@ export type AvatarProps = {
 }
 
 export default function Avatar({ user, onClick }: AvatarProps) {
-    return user
-        ? <img className="avatar" onClick={onClick} src={user.photoURL || `https://i.pravatar.cc/100?u=${user.uid}`} alt={user.name} />
-        : <img className="avatar" onClick={onClick} src="https://i.pravatar.cc/100" />
+    return <img className="avatar" onClick={onClick} src={user ? user.photoURL || `https://i.pravatar.cc/100?u=${user.uid}` : 'https://i.pravatar.cc/100'} alt={user?.name} />
 }
