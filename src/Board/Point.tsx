117225d85c599faf857392d768e17b179a9d10f1
Dean Sofer <ProLoser@users.noreply.github.com>
2024-10-12 23:47:18 +0000

    Removed hover styles and fixed tap/drag bug

--- src/Board/Point.tsx
+++ src/Board/Point.tsx
@@ -1,4 +1,4 @@
-import { useCallback, type DragEventHandler } from "react";
+import { useCallback, useState, type DragEventHandler } from "react";
 import Piece from './Piece'
 
 type PointProps = {
@@ -10,6 +10,7 @@ type PointProps = {
 }
 
 export default function Point({ pieces, move, position, onSelect, selected }: PointProps) {
+    const [isDragging, setIsDragging] = useState(false);
     const onDragOver: DragEventHandler = useCallback((event) => { event.preventDefault(); }, [])
     const onDrop: DragEventHandler = useCallback((event) => {
         event.preventDefault();
@@ -20,12 +21,27 @@ export default function Point({ pieces, move, position, onSelect, selected }: Po
 
     const color = pieces > 0 ? 'white' : 'black';
 
+    const handleDragStart = () => {
+        setIsDragging(true);
+    };
+
+    const handleDragEnd = () => {
+        setIsDragging(false);
+    };
+
     const onPointerUp = useCallback(() => {
+        if (isDragging) return;
         if (pieces !== 0 || selected !== null)
             onSelect(position)
-    }, [pieces, position, onSelect])
+    }, [pieces, position, onSelect, isDragging])
 
-    return <div className={`point ${selected === position ? 'selected' : ''}`} onPointerUp={onPointerUp} onDragOver={onDragOver} onDrop={onDrop}>
+    return <div className={`point ${selected === position ? 'selected' : ''}`} 
+        onPointerUp={onPointerUp} 
+        onDragOver={onDragOver} 
+        onDrop={onDrop}
+        onDragStart={handleDragStart}
+        onDragEnd={handleDragEnd}
+        >
         {Array.from({ length: Math.abs(pieces) }, (_, index) => <Piece key={index} color={color} position={position} onSelect={onSelect} />)}
     </div>
 }
