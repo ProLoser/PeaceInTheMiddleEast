157041ef071e068197c33785151d4747c25fe850
deansofer <deansofer@gmail.com>
2025-06-01 15:57:31 +0000

    Google AI studio tweaks

---
+++ .idx/dev.nix
@@ -0,0 +1,27 @@
+{pkgs}: {
+  channel = "stable-24.05";
+  packages = [
+    pkgs.nodejs_20
+    pkgs.corepack_latest
+  ];
+  idx.extensions = [
+    "svelte.svelte-vscode"
+    "vue.volar"
+  ];
+  idx.previews = {
+    previews = {
+      web = {
+        command = [
+          "yarn"
+          "start"
+          "--"
+          "--port"
+          "$PORT"
+          "--host"
+          "0.0.0.0"
+        ];
+        manager = "web";
+      };
+    };
+  };
+}
