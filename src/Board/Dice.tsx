644ab3ed3113da37aa8a7a045fb73c55ba67e944
Dean Sofer <Dean.Sofer@sony.com>
2025-06-21 22:42:16 +0300

    Dice used support

--- src/Board/Dice.tsx
+++ src/Board/Dice.tsx
@@ -6,20 +6,35 @@ import { Color } from '../Types';
 type DiceProps = {
     onPointerUp: PointerEventHandler,
     values: [number?, number?],
-    color?: Color,
     used: number[],
+    color?: Color,
 }
 
-export default function Dice({ onPointerUp, values, color, used = [] }: DiceProps) {
-    const left = `${color && color || Color.Black}${values[0] || 6}` as keyof typeof IMAGES
-    const right = `${color && color || Color.White}${values[1] || 6}` as keyof typeof IMAGES
-    // TODO: add class to used dice, may need to create array and map over it like if 3 of 4 6s are used
+export default function Dice({ onPointerUp, values = [6, 6], used = [], color }: DiceProps) {
+    let dice = [values[0]!, values[1]!];
+    
+    if (color && values[0] === values[1])
+        dice.push(values[0]!, values[1]!);
+    
+    const usedClone = [...used];
+    
     return <div className="dice" onPointerUp={onPointerUp}>
-        <img src={IMAGES[left]} />
-        {color && values[0] === values[1] && <>
-            <img src={IMAGES[left]} />
-            <img src={IMAGES[right]} />
-        </>}
-        <img src={IMAGES[right]} />
+        {dice.map((value, index) => {
+            const diceColor = color || (index % 2 === 0 ? Color.Black : Color.White);
+            const src = `${diceColor}${value}` as keyof typeof IMAGES;
+            const usedIndex = usedClone.indexOf(value);
+            const isUsed = ~usedIndex;
+            
+            if (isUsed)
+                usedClone.splice(usedIndex, 1);
+            
+            return (
+                <img 
+                    key={index}
+                    src={IMAGES[src]} 
+                    className={isUsed ? 'used' : ''} 
+                />
+            );
+        })}
     </div>
 }
