09a5b70ffa18198dbb5e761eefcf538983545006
Dean Sofer <ProLoser@users.noreply.github.com>
2025-08-07 19:16:50 -0700

    Update Avatar.tsx

--- src/Avatar.tsx
+++ src/Avatar.tsx
@@ -6,5 +6,8 @@ export type AvatarProps = {
 }
 
 export default function Avatar({ user }: AvatarProps) {
-    return <img className="avatar" src={user ? user.photoURL || `https://i.pravatar.cc/100?u=${user.uid}` : 'https://i.pravatar.cc/100'} alt={user?.name} />
+    return <img className="avatar"
+      src={user ? user.photoURL || `https://i.pravatar.cc/100?u=${user.uid}` : 'https://i.pravatar.cc/100'}
+      alt={user?.name}
+      draggable={false} />
 }
