624cba1b07175b3bb1f54fabe670543bcfc68c97
Dean Sofer <Dean.Sofer@sony.com>
2025-11-16 04:02:04 -0800

    Don't return

--- functions/index.js
+++ functions/index.js
@@ -65,7 +65,7 @@ exports.sendMoveNotification = onValueCreated('/moves/{moveId}', async event => 
   };
 
   try {
-    return console.log('Multicasting move', message)
+    console.log('Multicasting move', message)
     const response = await admin.messaging().sendEachForMulticast(message);
     
     response.responses.forEach((resp, idx) => {
